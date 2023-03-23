922d735dcaed644f1c7d1e23f6e4fd774139bf03
Hyeon-zero <gusdud9124@gmail.com>
2023-03-23 11:32:43 +0900

    Modify search text

--- js/search.js
+++ js/search.js
@@ -15,7 +15,10 @@ searchInput.addEventListener("keypress", (event) => {
     const url = `https://www.google.com/search?q=${query}`; // 구글로 이동하여 검색 결과 보여줌
 
     window.location.href = url;
-    searchInput.value = '';
-    searchInput.style.color = '#fff';
+    /* autocomplete="off"으로 인해 필요 없는 코드 */ 
+    // searchInput.value = '';
+    // searchInput.style.color = '#fff';
   }
 });
+
+// 검색창에 입력했으면 커서 위치 이동해도 사라지지 않게 코드 업데이트 필요
